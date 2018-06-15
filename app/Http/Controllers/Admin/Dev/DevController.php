aa971e4b5fda6260f25b02c701d6f42b50b9347c
rlrlaa123 <kimdonghyun3366@gmail.com>
2018-06-15 15:28:21 +0900

    development: 개발정보사업 edit, index, create

--- app/Http/Controllers/Admin/Dev/DevController.php
+++ app/Http/Controllers/Admin/Dev/DevController.php
@@ -31,12 +31,18 @@ class DevController extends Controller
      */
     public function index()
     {
+        $dev = Dev::all();
+        return view('admin.dev.dev_info.index', compact('dev'));
+    }
+
+    public function create()
+    {
         $locations = Location::all();
-//        $chosen=Location::where('dev_city','$chosen')->get();
         $cities = Location::distinct()->get(['dev_city']);
         $searchtype = SearchType::all();
         $searchcharge = SearchCharge::all();
-        return view('admin.dev.dev_info.index', compact('locations', 'searchtype', 'searchcharge', 'cities'));
+
+        return view('admin.dev.dev_info.create', compact('locations', 'searchtype', 'searchcharge', 'cities'));
     }
 
     public function developmentfileupload(Request $request)
@@ -122,7 +128,56 @@ class DevController extends Controller
         $dev->save();
 
         return redirect('admin/dev');
-
     }
 
+    public function edit($id)
+    {
+        $data = Dev::where('dev_id', $id)->get()[0];
+        $locations = Location::all();
+        $cities = Location::distinct()->get(['dev_city']);
+        $searchtype = SearchType::all();
+        $searchcharge = SearchCharge::all();
+
+        return view('admin.dev.dev_info.edit', compact('data', 'locations', 'searchtype', 'searchcharge', 'cities'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'dev_title' => 'required|string',
+            'dev_initiated_log' => 'required',
+            'dev_initiated_date' => 'required|date',
+            'dev_comment' => 'required',
+            'dev_city' => 'required',
+            'dev_district' => 'required',
+            'dev_type' => 'required',
+            'dev_charge' => 'required',
+            'dev_status' => 'required',
+            'dev_method' => 'required',
+            'dev_area_size' => 'required|integer',
+            'dev_applied_law' => 'nullable|string',
+            'dev_publicly_starting_date' => 'required|string',
+            'dev_future_plan' => 'nullable',
+        ]);
+
+        $dev = Dev::where('dev_id', $id)
+            ->update([
+                'dev_title' => $request['dev_title'],
+                'dev_initiated_log' => $request['dev_initiated_log'],
+                'dev_initiated_date' => $request['dev_initiated_date'],
+                'dev_comment' => $request['dev_comment'],
+                'dev_city' => $request['dev_city'],
+                'dev_district' => $request['dev_district'],
+                'dev_type' => $request['dev_type'],
+                'dev_charge' => $request['dev_charge'],
+                'dev_status' => $request['dev_status'],
+                'dev_method' => $request['dev_method'],
+                'dev_area_size' => $request['dev_area_size'],
+                'dev_applied_law' => $request['dev_applied_law'],
+                'dev_publicly_starting_date' => $request['dev_publicly_starting_date'],
+                'dev_future_plan' => $request['dev_future_plan'],
+            ]);
+
+        return redirect('admin/dev');
+    }
 }
