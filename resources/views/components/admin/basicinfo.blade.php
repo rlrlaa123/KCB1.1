b422d8ef04d7289932167946a4adc97c941297fd
bosung2697 <36359088+bosung2697@users.noreply.github.com>
2018-06-15 17:50:01 +0900

    admin_basicinfo-selector:요청사항 없어서 그냥 없어짐

--- resources/views/components/admin/basicinfo.blade.php
+++ resources/views/components/admin/basicinfo.blade.php
@@ -1,13 +1,13 @@
-{{--<div class="sidebar basicinfo-selector {{ preg_match('/\/admin\/basic/', $_SERVER['REQUEST_URI']) ? '' : 'hide'}}">--}}
-    {{--<div class="sidemenu">기초정보</div>--}}
-    {{--<div class="sideuser">관리자</div>--}}
-    {{--<div class="sidesubmenu">--}}
-        {{--<ul>--}}
-            {{--<li class="liactive"> 사이트정보</li>--}}
-            {{--<li> 관리자정보</li>--}}
-            {{--<li> 입금계좌</li>--}}
-            {{--<li> 사이트정책</li>--}}
+<div class="sidebar basicinfo-selector {{ preg_match('/\/admin\/basic/', $_SERVER['REQUEST_URI']) ? '' : 'hide'}}">
+    <div class="sidemenu">기초정보</div>
+    <div class="sideuser">관리자</div>
+    <div class="sidesubmenu">
+        <ul>
+            <li class="liactive"> 사이트정보</li>
+            <li> 관리자정보</li>
+            <li> 입금계좌</li>
+            <li> 사이트정책</li>
 
-        {{--</ul>--}}
-    {{--</div>--}}
-{{--</div>--}}
+        </ul>
+    </div>
+</div>
