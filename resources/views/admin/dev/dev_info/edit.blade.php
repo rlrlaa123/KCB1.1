476e93ff36aff3303210d990d24b4749a3a9f912
rlrlaa123 <kimdonghyun3366@gmail.com>
2018-06-26 21:54:19 +0900

    dev_district 데이터 불러올 수 있도록 수정

--- resources/views/admin/dev/dev_info/edit.blade.php
+++ resources/views/admin/dev/dev_info/edit.blade.php
@@ -39,7 +39,8 @@
                             <td class="datainput"><label for="dev_title">제목</label></td>
                             <td>
                                 <input type="text" id="dev_title" name="dev_title" class="form-control"
-                                       placeholder="제목을 입력해주세요." size="68" value="{{ old('dev_title',$data->dev_title) }}">
+                                       placeholder="제목을 입력해주세요." size="68"
+                                       value="{{ old('dev_title',$data->dev_title) }}">
                                 @if ($errors->has('dev_title'))
                                     <div class="help-block">
                                         {{ $errors->first('dev_title') }}
@@ -51,7 +52,8 @@
                             <td class="datainput"><label for="dev_initiated_log">추진내역</label></td>
                             <td>
                                 <input type="text" id="dev_initiated_log" name="dev_initiated_log" class="form-control"
-                                       placeholder="추진내역을 입력해주세요." size="68" value="{{ old('dev_initiated_log',$data->dev_initiated_log)}}">
+                                       placeholder="추진내역을 입력해주세요." size="68"
+                                       value="{{ old('dev_initiated_log',$data->dev_initiated_log)}}">
                                 @if ($errors->has('dev_initiated_log'))
                                     <div class="help-block">
                                         {{ $errors->first('dev_initiated_log') }}
@@ -63,7 +65,8 @@
                             <td class="datainput"><label for="dev_initiated_date">추진내역날짜</label></td>
                             <td>
                                 <input type="date" id="dev_initiated_date" name="dev_initiated_date"
-                                       class="form-control" size="68" value="{{ old('dev_initiated_date', $data->dev_initiated_date) }}">
+                                       class="form-control" size="68"
+                                       value="{{ old('dev_initiated_date', $data->dev_initiated_date) }}">
                                 @if ($errors->has('dev_initiated_date'))
                                     <div class="help-block">
                                         {{ $errors->first('dev_initiated_date') }}
@@ -75,7 +78,8 @@
                             <td class="datainput"><label for="dev"></label>COMMENT</td>
                             <td>
                                 <textarea id="dev_comment" name="dev_comment" class="form-control"
-                                          placeholder="COMMENT를 입력해주세요." cols="70">{{ old('dev_comment',$data->dev_comment) }}</textarea>
+                                          placeholder="COMMENT를 입력해주세요."
+                                          cols="70">{{ old('dev_comment',$data->dev_comment) }}</textarea>
                                 @if ($errors->has('dev_comment'))
                                     <div class="help-block">
                                         {{ $errors->first('dev_comment') }}
@@ -84,14 +88,16 @@
                             </td>
                         </tr>
                         <tr>
-                            <td class="datainput"><label for="dev_city">시/도</label><label for="dev_district">군/구 {{ old('dev_district', $data->dev_district) }}</label>
+                            <td class="datainput"><label for="dev_city">시/도</label><label
+                                        for="dev_district">군/구 {{ old('dev_district', $data->dev_district) }}</label>
                             </td>
                             <td>
                                 <select id="dev_city" name="dev_city" form="development-form"
                                         onchange="showDistrictList(value, '{{  $data->dev_city }}')">
                                     <option value={{ $data->dev_city }} selected>선택해주세요</option>
                                     @foreach($cities as $city)
-                                        <option class="listed" value={{ $city->dev_city }} @if($data->dev_city == $city->dev_city) selected @endif>{{ $city->dev_city }}</option>
+                                        <option class="listed"
+                                                value={{ $city->dev_city }} @if($data->dev_city == $city->dev_city) selected @endif>{{ $city->dev_city }}</option>
                                     @endforeach
                                 </select>
                                 @if ($errors->has('dev_city'))
@@ -101,10 +107,11 @@
                                 @endif
                                 <select id="dev_district" name="dev_district" form="development-form">
                                     @foreach($locations as $loc)
-                                        <option class="listed2" value="{{ $data->dev_district  }}"
-                                                id="{{ $loc->num_id }}"
-                                                style="display: none">
-                                            {{$loc->dev_district}}
+                                        <option class="listed2"
+                                                value={{ $loc->dev_district  }} @if($data->dev_district == $loc->dev_district) selected
+                                                @endif
+                                                id="{{ $loc->num_id }}">
+                                            {{ $loc->dev_district }}
                                         </option>
                                     @endforeach
                                 </select>
@@ -116,7 +123,8 @@
                             </td>
                         </tr>
                         <tr>
-                            <td class="datainput"><label for="dev_type">유형 {{old('dev_type',$data->dev_type)}}</label></td>
+                            <td class="datainput"><label for="dev_type">유형 {{old('dev_type',$data->dev_type)}}</label>
+                            </td>
                             <td>
                                 <select id="dev_type" name="dev_type" form="development-form">
                                     @foreach($searchtype as $type)
@@ -149,7 +157,8 @@
                             <td class="datainput"><label for="dev_area_size">면적 (m<sup>2</sup>)</label></td>
                             <td>
                                 <input type="number" id="dev_area_size" name="dev_area_size"
-                                       class="form-control" size="68" value="{{ old('dev_area_size', $data->dev_area_size) }}"
+                                       class="form-control" size="68"
+                                       value="{{ old('dev_area_size', $data->dev_area_size) }}"
                                        placeholder="면적을 입력 요.">
                                 @if ($errors->has('dev_area_size'))
                                     <div class="help-block">
@@ -178,7 +187,8 @@
                             <td class="datainput"><label for="dev_method">사업 방식</label></td>
                             <td>
                                 <input type="text" id="dev_method" name="dev_method" class="form-control"
-                                       placeholder="사업방식을 입력해주세요." size="68" value="{{ old('dev_method',$data->dev_method) }}">
+                                       placeholder="사업방식을 입력해주세요." size="68"
+                                       value="{{ old('dev_method',$data->dev_method) }}">
                                 @if ($errors->has('dev_method'))
                                     <div class="help-block">
                                         {{ $errors->first('dev_method') }}
@@ -190,7 +200,8 @@
                             <td class="datainput"><label for="dev_applied_law">적용 법률</label></td>
                             <td>
                                 <input type="text" id="dev_applied_law" name="dev_applied_law" class="form-control"
-                                       placeholder="적용법률을 입력해주세요." size="68" value="{{ old('dev_applied_law',$data->dev_applied_law) }}">
+                                       placeholder="적용법률을 입력해주세요." size="68"
+                                       value="{{ old('dev_applied_law',$data->dev_applied_law) }}">
                                 @if ($errors->has('dev_applied_law'))
                                     <div class="help-block">
                                         {{ $errors->first('dev_applied_law') }}
@@ -202,7 +213,8 @@
                             <td class="datainput"><label for="dev_publicly_starting_date">사업인정고시일</label></td>
                             <td>
                                 <input type="date" id="dev_publicly_starting_date" name="dev_publicly_starting_date"
-                                       class="form-control" value="{{ old('dev_publicly_starting_date', $data->dev_publicly_starting_date) }}">
+                                       class="form-control"
+                                       value="{{ old('dev_publicly_starting_date', $data->dev_publicly_starting_date) }}">
                                 @if ($errors->has('dev_publicly_starting_date'))
                                     <div class="help-block">
                                         {{ $errors->first('dev_publicly_starting_date') }}
@@ -226,7 +238,8 @@
                         <tr>
                             <td class="datainput"><label for="dev_reference">참고 자료</label></td>
                             <td>
-                                <input type="file" id="dev_reference" name="dev_reference" class="image"                                      value="{{ old('dev_reference', $data->dev_reference)}}">
+                                <input type="file" id="dev_reference" name="dev_reference" class="image"
+                                       value="">
                                 @if ($errors->has('dev_reference'))
                                     <div class="help-block">
                                         {{ $errors->first('dev_reference') }}
@@ -238,7 +251,7 @@
                             <td class="datainput"><label for="dev_fileimage">위치 이미지</label></td>
                             <td>
                                 <input type="file" id="dev_fileimage" name="dev_fileimage" class="image"
-                                       value="{{ old('dev_fileimage',$data->dev_fileimage) }}">
+                                       value="">
                                 @if ($errors->has('dev_fileimage'))
                                     <div class="help-block">
                                         {{ $errors->first('dev_fileimage') }}
@@ -312,7 +325,7 @@
                     console.log(ele['num_id']);
                     document.getElementById(ele['num_id']).value = ele['dev_district'];
                     document.getElementById(ele['num_id']).style.display = 'block';
-                    if(document.getElementById(ele['num_id']).value === selected) {
+                    if (document.getElementById(ele['num_id']).value === selected) {
                         document.getElementById('dev_district').selectedIndex = ele['num_id'];
                     }
                 })
