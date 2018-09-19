28515b4a4b5d83877a72b4a3440e0c36f947b64f
bosung2697 <36359088+bosung2697@users.noreply.github.com>
2018-09-19 14:17:30 +0900

    location added

--- resources/views/Intro/Company_location.blade.php
+++ resources/views/Intro/Company_location.blade.php
@@ -7,7 +7,8 @@
         <div>
             <div class="introductionPage">
                 <h3 style="font-size: 1.5vw; font-weight: 700; color: black; text-align: left; margin: 4vh 0;">회사 위치</h3>
-                <img src="img/location.jpg" style="width:100%;">
+                <a href="https://map.naver.com/?__pinOnly=false&perimeter=1&query=&searchCoord=&menu=location&tab=1&lng=eca40e4a873f891e12621ef4d07ba61e&__fromRestorer=true&mapMode=0&mpx=1e5726e1cf9288a573300671df225b3a1c83781ddbdf7ebbd51aedd89ad4a193cd7249621fcc7abc257d94d34219b0d8&pinId=31132214&pinType=site&lat=282d89648c91ecab89ede8e10786771a&dlevel=11&enc=b64" target="_blank">
+                <img src="img/location.jpg" style="width:100%;"></a>
             </div>
         </div>
     </div>
